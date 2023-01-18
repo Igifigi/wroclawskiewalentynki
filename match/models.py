50d268f49187d0c370a9cccce0d6bf2869e6d5fb
Ignacy Świderski <44808028+Igifigi@users.noreply.github.com>
2023-01-18 19:55:30 +0100

    Create School model

--- match/models.py
+++ match/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+class School(models.Model):
+    name = models.CharField(_("Name"), max_length=100)
+    code = models.CharField(_("Code"), max_length=3) # TODO: set code to exactly 3 znaki
+
