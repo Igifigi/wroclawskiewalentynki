8c95a520e7217f555bb234e8db545c2d0313eb02
Igifigi <ignacyswiderski@wp.pl>
2023-01-19 14:38:32 +0100

    Create create_profile view

--- match/views.py
+++ match/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.utils.translation import gettext as _
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import UserProfileForm
+
+@login_required
+def create_profil(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, _("Successfully created profile."))
+            print('mess2') #DEBUG
+            return redirect('index') # TODO: change
+        messages.error(request, _("Unsuccessful")) # TODO: make error message
+        form = UserProfileForm()
+        context = {
+            "form": form
+        }
+        return render(request, template_name='create_user_profile.html', context=context)
