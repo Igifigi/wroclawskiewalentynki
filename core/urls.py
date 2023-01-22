bf1e61372aa424264d6e51058374238c6fe53abe
Igifigi <ignacyswiderski@wp.pl>
2023-01-22 21:12:24 +0100

    Reformat core urls

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('login', login_request, name='login'),
-    path('logout', logout_request, name='logout'),
-    path('register', register_request, name='register'),
-    path('chat', index, name='chat'),
+    path('login/', login_request, name='login'),
+    path('logout/', logout_request, name='logout'),
+    path('register/', register_request, name='register'),
+    path('chat/', index, name='chat'),
 ]
