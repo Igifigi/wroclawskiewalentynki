6306164f2ff3850983c8849e821ecaa3acda89f6
Ignacy Świderski <44808028+Igifigi@users.noreply.github.com>
2023-01-26 19:06:27 +0100

    Introduce flexible_form.html and edit_profile view

--- match/views.py
+++ match/views.py
@@ -15,6 +15,7 @@ def create_profile(request):
         if form.is_valid():
             object = form.save(commit=False)
             object.user = request.user
+            object.matched = False
             object.save()
             messages.success(request, _("Successfully created profile."))
             return redirect('index') # TODO: change
@@ -22,6 +23,31 @@ def create_profile(request):
     else:
         form = UserProfileForm()
     context = {
-        "form": form
+        'form': form,
+        'form_name': _('Create your profile'),
+        'form_title': _('Create your profile'),
+        'submit_button_label': _('Submit'),
     }
-    return render(request, template_name='create_user_profile.html', context=context)
+    return render(request, template_name='flexible_form.html', context=context)
+
+@login_required
+def edit_profile(request):
+    if not UserProfile.objects.filter(user=request.user):
+        return redirect('create_profile')
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=UserProfile.objects.get(user=request.user))
+        if form.is_valid():
+            form.save()
+            messages.success(request, _('Successfully updated profile.'))
+            return redirect('index') # TODO: change
+        messages.error(request, _('Unsuccessful')) # TODO: make error message
+    else:
+        form = UserProfile(instance=UserProfile.objects.get(user=request.user))
+    context = {
+        'form': form,
+        'form_name': _('Edit your profile'),
+        'form_title': _('Edit your profile'),
+        'submit_button_label': _('Submit'),
+    }
+    return render(request, template_name='flexible_form.html', context=context)
+    
