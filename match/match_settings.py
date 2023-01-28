ae3336ed57fc71c37b057dbb01e03a06a1c5f65d
Ignacy Świderski <44808028+Igifigi@users.noreply.github.com>
2023-01-28 23:22:04 +0100

    Add match arrays

--- match/match_settings.py
+++ match/match_settings.py
@@ -66,16 +66,16 @@ class Q8(models.IntegerChoices):
 
 # On a scale of 1 to 10, how decisive are you?
 class Q9(models.IntegerChoices):
-    ONE = 1,
-    TWO = 2,
-    THREE = 3,
-    FOUR = 4,
-    FIVE = 5,
-    SIX = 6,
-    SEVEN = 7,
-    EIGHT = 8,
-    NINE = 9,
-    TEN = 10,
+    ONE = 1, '1',
+    TWO = 2, '2',
+    THREE = 3, '3',
+    FOUR = 4, '4',
+    FIVE = 5, '5',
+    SIX = 6, '6',
+    SEVEN = 7, '7',
+    EIGHT = 8, '8',
+    NINE = 9, '9',
+    TEN = 10, '10',
 
 # Dogs or perhaps cats?
 class Q10(models.IntegerChoices):
@@ -107,3 +107,89 @@ class Q13(models.IntegerChoices):
     FOREIGN_LANGUAGES = 6, _('Foreign Languages'),
     BIOLOGY_CHEMISTRY = 7, _('Biology/Chemistry'),
     PE = 8, _('PE'),
+
+Q1_matches = [
+    (1,1),
+    (2,2),
+    (2,3),
+    (3,3),
+    (4,4),
+]
+
+Q8_matches = [
+    (1,1),
+    (2,2),
+    (3,3),
+    (4,4),
+    (5,5),
+    (6,6),
+    (7,7),
+    (8,8),
+    (9,9),
+    (10,10),
+    (11,11),
+    (12,12),
+    (13,13),
+    (1,3),
+    (1-5),
+    (2-9),
+    (4-6),
+    (4-8),
+    (7,12),
+    (9,13),
+    (10,12),
+    (12,13),
+]
+
+Q9_matches = [
+    (1,1),
+    (2,2),
+    (3,3),
+    (4,4),
+    (5,5),
+    (6,6),
+    (7,7),
+    (8,8),
+    (9,9),
+    (10,10),
+    (1,2),
+    (1,3),
+    (2,3),
+    (4,5),
+    (4,6),
+    (5,6),
+    (7,8),
+    (7,9),
+    (7,10),
+    (8,9),
+    (8,10),
+    (9,10),
+]
+
+Q10_matches = [
+    (1,1),
+    (2,2),
+    (3,3),
+    (4,4),
+    (1,4),
+    (2,4),
+]
+
+Q11_matches = [
+    (1,1),
+    (2,2),
+    (3,3),
+    (4,4),
+    (5,5),
+    (2,3),
+    (2,4),
+    (3,4),
+]
+
+Q13_matches = [
+    (1,4),
+    (2,5),
+    (3,7),
+    (6,6),
+    (7,7),
+]
