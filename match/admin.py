da8bbd0cb92d43cfd9b1338f082718e2250324f3
Igifigi <ignacyswiderski@wp.pl>
2023-02-07 19:46:38 +0100

    Fix school filter in admin panel

--- match/admin.py
+++ match/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
-from django.contrib.admin import ModelAdmin
+from django.contrib.admin import ModelAdmin, SimpleListFilter
+from django.utils.translation import gettext as _
+
 from .models import UserProfile, School, Match
 
 admin.site.register(School)
 admin.site.register(Match)
 
+class SchoolCodeFilter(SimpleListFilter):
+    title = _('School code')
+    parameter_name = 'school_code'
+    
+    def lookups(self, request, model_admin):
+        return [(school.code, school.name) for school in School.objects.all()]
+    
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            return queryset
+        return queryset.filter(school=self.value())
+
 @admin.register(UserProfile)
 class UserProfileAdmin(ModelAdmin):
-    list_filter = ('school',)
+    list_filter = (SchoolCodeFilter,)
