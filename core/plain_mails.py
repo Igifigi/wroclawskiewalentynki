d406c5faed065b2f9a38dd8932e47306471b0f2b
Igifigi <ignacyswiderski@wp.pl>
2023-02-11 11:09:55 +0100

    Reset password feature

--- core/plain_mails.py
+++ core/plain_mails.py
@@ -1,17 +1,32 @@
 ﻿from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 
-def validate_email_message(first_name, last_name, uid, token):
+def validate_email_message(uid, token):
     return {
         'subject': _('Verify e-mail address'),
         'message': _(
-        'Hello! ' +
-        'You have received this message because an instruction to create an account on the wroclawskiewalentynki.pl website has been issued. ' +
-        'Please copy and paste the following URL into your web browser to verify your e-mail address: ' +
-        f'{settings.WWW_SITE}/validate/{uid}/{token}. ' +
-        'If you did not issue the instruction, write an e-mail to kontakt@wroclawskiewalentynki.pl. ' +
-        'Thank you for being part of our community. ' +
-        'Team of Wroclaw Valentine\'s Day 2023 ' +
-        'Message generated automatically. Please do not reply to it.'
+            'Hello! ' +
+            'You have received this message because an instruction to create an account on the wroclawskiewalentynki.pl website has been issued. ' +
+            'Please copy and paste the following URL into your web browser to verify your e-mail address: ' +
+            f'{settings.WWW_SITE}/validate/{uid}/{token}. ' +
+            'If you did not issue the instruction, write an e-mail to kontakt@wroclawskiewalentynki.pl. ' +
+            'Thank you for being part of our community. ' +
+            'Team of Wroclaw Valentine\'s Day 2023 ' +
+            'Message generated automatically. Please do not reply to it.'
+        )
+    }
+
+def reset_password_message(uid, token):
+    return {
+        'subject': _('Reset your password'),
+        'message': _(
+            'Hello! ' +
+            'You have received this message because an instruction to change the password from your account at wroclawskiewalentynki.pl has been issued. ' +
+            'Please copy and paste the following URL into your web browser to verify your e-mail address: ' +
+            f'{settings.WWW_SITE}/validate/{uid}/{token}. ' +
+            'If you did not issue the instruction, write an e-mail to kontakt@wroclawskiewalentynki.pl. ' +
+            'Thank you for being part of our community. ' +
+            'Team of Wroclaw Valentine\'s Day 2023 ' +
+            'Message generated automatically. Please do not reply to it.'
         )
     }
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 ﻿from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView
+from django.utils.translation import gettext as _
 
 from .views import *
 
@@ -13,4 +15,38 @@ urlpatterns = [
     path('terms_of_use', show_terms, name='terms'),
     path('privacy_policy', show_privacy_policy, name='privacy'),
     path('validate/<uidb64>/<token>', validate_email, name='validate_email'),
-] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path(
+        'reset_password/',
+        PasswordResetView.as_view(
+            template_name='password_reset.html',
+            html_email_template_name='emails/password_reset.html',
+            subject_template_name='emails/password_reset_subject.txt'
+        ),
+        name='password_reset'
+    ),
+    path(
+        'reset_password/done/',
+        message_and_redirect,
+        {
+            'message': _('If there is an account assigned to the given email address, instructions for changing the password have been sent.'),
+            'redirect_url': 'index',
+        },
+        name='password_reset_done'
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(
+            template_name='password_reset_confirm.html',
+        ),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset_password/complete',
+        message_and_redirect,
+        {
+            'message': _('Password changed successfully. You can now log in.'),
+            'redirect_url': 'login',
+        },
+        name='password_reset_complete'
+    ),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
